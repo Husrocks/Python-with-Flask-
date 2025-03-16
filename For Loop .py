2409205fbc39b6f7b2e4308233bb40f1b7133b14
Husrocks <zurqulnan@gmail.com>
2025-03-16 21:09:35 +0500

    Add files via upload

---
+++ For Loop .py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+word = "Python"
+for i in word:
+    print(i)
+for i in range(2, 10):  # Starts from 2 and goes up to 9
+    print(i)
+for i in range(2, 10, 2):  # Starts from 2, increments by 2, stops before 10
+    print(i)
